0f55b2b394545ca0cdd25be4b84383c1457bbd75
kyh <kyh_0625@naver.com>
2025-10-29 10:59:39 +0900

    매물번호 입력

--- src/app/(app)/landSearch/ListingCard.tsx
+++ src/app/(app)/landSearch/ListingCard.tsx
@@ -87,6 +87,9 @@ const ListingCard = ({ listing, onClick }: Props & { onClick: (id: number) => vo
               </div>
             )}
           </div>
+          <div className="absolute top-2 right-2 bg-black bg-opacity-50 text-white px-2 py-1 text-xs rounded">
+            #{listing.id}
+          </div>
         </div>
 
         {/* Content Section */}
